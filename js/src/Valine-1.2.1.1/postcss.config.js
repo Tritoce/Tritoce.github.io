80b4dec5ff42feb998c488743ec4510d705f33f9
Tritoce <xtghdesu@qq.com>
2021-11-10 12:38:05 +0800

    Site updated: 2021-11-10 12:38:05

--- js/src/Valine-1.2.1.1/postcss.config.js
+++ js/src/Valine-1.2.1.1/postcss.config.js
@@ -1,3 +1,3 @@
-// build time:Mon Nov 08 2021 20:22:59 GMT+0800 (中国标准时间)
+// build time:Wed Nov 10 2021 12:38:03 GMT+0800 (中国标准时间)
 module.exports={plugins:[require("autoprefixer")]};
 //rebuild by hrmmi 
